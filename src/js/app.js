dbcf5bd668fef7aceef38638966e94d367168918
Andrii <andrejsalogub3@gmail.com>
2024-01-17 13:37:31 +0200

    Enhanced game functionality with round counter and opponent move display

--- src/js/app.js
+++ src/js/app.js
@@ -2,9 +2,19 @@ const moveVariants = document.querySelectorAll(".game-interface__user-move");
 let playerMove = "";
 let enemyMove = "";
 
+const enemyMoveImage = document.querySelector(
+    ".game-interface__opponent-choose"
+);
+
+let round = 0;
+const roundCounter = document.querySelector(".game-interface__rounds");
+
 moveVariants.forEach((move) => {
-    move.addEventListener("click", (e) => {
+    move.addEventListener("click", () => {
         playerMove = move.getAttribute("id");
+        generateRandomEnemyMove();
+        round += 1;
+        roundCounter.textContent = ` Round: ${round}`;
     });
 });
 
@@ -13,5 +23,6 @@ function generateRandomEnemyMove() {
         moveVariants[
             Math.floor(0 + Math.random() * (moveVariants.length - 0))
         ].getAttribute("id");
-    console.log(enemyMove);
+
+    enemyMoveImage.setAttribute("src", `src/images/${enemyMove}.png`);
 }
